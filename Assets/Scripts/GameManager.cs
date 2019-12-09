912d0d48b146ee405a615511e14b4ac5a08fcf4f
Kocot <monika.kocot@capgemini.com>
2019-12-09 23:54:44 +0100

    Adding game package

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+
+    public static GameManager instance;
+    public bool gameOver;
+    // Start is called before the first frame update
+
+    void Awake()
+    {
+        if (instance == null)
+        {
+            instance = this;
+        }
+    }
+    void Start()
+    {
+        gameOver = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void StartGame()
+    {
+        UiManager.instance.GameStart();
+        ScoreManager.instance.StartScore();
+        GameObject.Find("PlatformSpawner").GetComponent<PlatformSpawner>().StartSpawningPlatforms();
+    }
+
+    public void GameOver()
+    {
+        UiManager.instance.GameOver();
+        ScoreManager.instance.StopScore();
+    }
+}
---
+++ Assets/Scripts/ScoreManager.cs
@@ -0,0 +1,59 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ScoreManager : MonoBehaviour
+{
+    public static ScoreManager instance;
+    public int score;
+    public int highScore;
+    // Start is called before the first frame update
+
+    void Awake()
+    {
+        if(instance == null)
+        {
+            instance = this;
+        }
+    }
+
+    void Start()
+    {
+        score = 0;
+        PlayerPrefs.SetInt("score", score);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void incrementScore()
+    {
+        score += 1;
+    }
+
+    public void StartScore()
+    {
+        InvokeRepeating("incementScore", 0.1f, 0.5f);   
+    }
+
+    public void StopScore()
+    {
+        CancelInvoke("incrementScore");
+        PlayerPrefs.SetInt("score", score);
+
+        if (PlayerPrefs.HasKey("highScore"))
+        {
+            if(score > PlayerPrefs.GetInt("highScore"))
+            {
+                PlayerPrefs.SetInt("highScore", score);
+            }
+
+        } else
+        {
+            PlayerPrefs.SetInt("highScore", score);
+        }
+    }
+}
